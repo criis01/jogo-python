4ef995553eb883cbb5eb65246e561d2fd1732b53
unknown <crislaneandrade05062005@gmail>
2026-03-25 20:00:03 -0300

    corrigindo lógica e melhorando jogo

--- jogo.py
+++ jogo.py
@@ -1,13 +1,26 @@
 import random
 numero_secreto= random.randint(1,10)
+tentativas = 5
 
-while True:
-    palpite= int(input("tente adivinhar o número (1 a 10):"))
+print ("Bem vindos ao jogo de adivinhação!")
+print ("Tente adivinhar o número entre 1 e 10")
+print ("Você tem até 5 tentativas")
 
-    if palpite== numero_secreto:
+while tentativas > 0:
+    chute = int(input("digite um número:"))
+
+    if chute == numero_secreto:
         print("acertou!!")
-        break
-    elif palpite < numero_secreto:
-        print("tente um número MAIOR")
+        break 
+
+    elif chute > numero_secreto:
+        print("muito alto!")
     else:
-     print("tente um numero MENOR")
+     print("muito baixo!")
+
+      
+    tentativas -= 1
+    print(f"você ainda tem:{tentativas}")
+
+if tentativas == 0:
+   print (f"você perdeu! o número era  {numero_secreto}")
